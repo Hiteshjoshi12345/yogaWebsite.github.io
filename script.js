ad27975bae505dd82a1afdbf04e01262571428ec
Hiteshjoshi12345 <66710595+Hiteshjoshi12345@users.noreply.github.com>
2021-11-26 00:19:32 +0530

    Yoga Website.
    Yoga Website.

--- script.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-    setTimeout(function () {
-        var loader = document.getElementById('loading');
-        loader.style.display = "none";
-    }, 3000);
-}
-myFunction();
